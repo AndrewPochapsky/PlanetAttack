67b51c7df4e086b389fd624264276f83127b8d16
Andrew Pochapsky-LPTP <andrew.pochapsky@gmail.com>
2017-04-21 17:01:21 -0400

    Initial Commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+ 
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+        Debug.Log("Loading level: " + levelName);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Exiting Game");
+        Application.Quit();
+    } 
+
+
+}
