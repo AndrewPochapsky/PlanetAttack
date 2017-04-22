fd8fc65ffef0ac115b87433d789e3caff2d98fc9
Andrew Pochapsky-LPTP <andrew.pochapsky@gmail.com>
2017-04-22 14:33:17 -0400

    Have camps which can spawn as many slimes as desired

--- Assets/Scripts/Enemies/SpawnCamp.cs
+++ Assets/Scripts/Enemies/SpawnCamp.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnCamp : MonoBehaviour {
-
-    public Enemy[] enemyTypes;
-
+    [SerializeField]
+    private Enemy[] enemyTypes;
+    [SerializeField]
+    private int amountToSpawn;
+    [SerializeField][Tooltip("Spawn ever x amount of seconds")]
+    private float spawnSpeed;
 	// Use this for initialization
 	void Start () {
-		
+        StartCoroutine(SpawnEnemies());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private IEnumerator SpawnEnemies()
+    {
+        int randomEnemyIndex = Random.Range(0, enemyTypes.Length);
+        Enemy enemyToSpawn = enemyTypes[randomEnemyIndex];
+        for (int i = 0; i < amountToSpawn; i++)
+        {
+            yield return new WaitForSeconds(spawnSpeed);
+            Instantiate(enemyToSpawn, transform.position, transform.rotation);
+        }
+        print("SPAWNING");
+    }
+
 }
